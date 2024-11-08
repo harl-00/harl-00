66969121cf5f80c063b81917053269fc771edd3e
harl-00 <harleytickle001@gmail.com>
2024-11-08 14:18:17 +0000

    Assessment 3
    Grades python file

---
+++ grades.py
@@ -0,0 +1,13 @@
+mark = float(input("Student Mark: "))
+if mark >20 <29:
+    print("F")
+elif mark >30 <39:
+    print("E")
+elif mark >40 <49:
+    print("D")
+elif mark >50 <59:
+    print("C")
+elif mark >60 <69:
+    print("B")
+elif mark >70 <100:
+    print("A")

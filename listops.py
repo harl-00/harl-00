f28b9e739fcf98bee8678e93a32c5bfbb3f57f64
harl-00 <harleytickle001@gmail.com>
2024-11-22 14:05:48 +0000

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,14 @@
+list = [10, 20, 30, 40, 50]
+print(*list)
+
+all = sum(list)
+print (all)
+
+largest = max(list)
+print(largest)
+
+list.reverse()
+print (list)
+
+smallest =min(list)
+print(smallest)

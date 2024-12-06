702f9289ecffd1af6a4b36073b7147ebaf6ba061
harl-00 <harleytickle001@gmail.com>
2024-12-06 15:17:55 +0000

    Assessment 10

---
+++ buggy_code.py
@@ -0,0 +1,28 @@
+"""
+This code contains some bugs and some errors.  
+Find them and fix them.  
+When you are done the program will print a message.
+Write the message at the bottom of the code before submission
+"""
+deftract_and_rearrange(string)
+"""
+Removing not needed indentations
+"""
+str_1 = "".join(reversed(string[0:4].split('g'))).capitalize()
+str_2 = "".join(string[6:13].splt('ro'))
+str3 = join("".join(reversed(list(string[14:20]))).split(string[17]))
+str_4 = "".join(string[21:29].split(string[23:28]))
+
+return str_1 + " " + str_2 + " " + str_3 + " " + str_4
+"""
+Adding missing quotations to complete the return sequence
+"""
+
+def ultra_extrct_and_rearrange(string):
+    first_transform = extract_and_rearrange(string)
+    return first_transform
+"""
+Adding missing bracket towards the end
+"""
+print(ultra_extract_andrearrange("egthb quirock nwoGrb forijmpxv"))
+
